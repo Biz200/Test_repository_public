bdd5563fc9aa9d257ccb9e48c4adfc3206f422ba
Bogdan <bizik200@yandex.ru>
2022-12-05 20:42:57 +0700

    Test 2 complete

--- Test2/Program.cs
+++ Test2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите 3 числа:");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int number3 = int.Parse(Console.ReadLine());
+
+int max = number1;
+
+            if (number2 > max)
+            {
+                max = number2;
+            }
+
+            if (number3 > max)
+            {
+                max = number3;
+            }
+
+Console.WriteLine("Максимальное из трех чисел: " + max);
