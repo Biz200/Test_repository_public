ecc624f001dad67345eb150cece3408a70647175
Bogdan <bizik200@yandex.ru>
2022-12-05 20:32:18 +0700

    Test 1 complete

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+            {
+                Console.WriteLine("Первое число " + numberA + " больше чем второе " + numberB);
+            }
+            else
+            {
+                Console.WriteLine("Второе число " + numberB + " больше чем первое " + numberA);
+            }
